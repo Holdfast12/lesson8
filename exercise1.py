7b88f7a3b7b2c1ab51cd5be5cff392247cbda168
Mikhail Kalyujniy <mih.kalyujniy@yandex.ru>
2022-12-23 14:25:40 +0300

    123

--- exercise1.py
+++ exercise1.py
@@ -1,6 +1,41 @@
 #!/usr/bin/python3
-class animals:
-    def __init__(self, gender, age, weight):
-        self.age = age
+"""Написать класс “animals” (животные), который включает общие признаки и поведения животных.
+От “animals” наследовать класс “mammals” (млекопитающие), который включает общие признаки и поведения млекопитающих.
+От “mammals” наследовать “human” (человек), “cat”(кот), “dog”(собака), у каждого свои признаки и поведения.
+"""
+
+class Animals:
+    def __init__(self, weight, height, age):
         self.weight = weight
-        
+        self.height = height
+        self.age = age
+
+class Mammals(Animals):
+    pass
+
+class Human(Mammals):
+    def __init__(self, weight, height, age, name, gender):
+        super().__init__(weight, height, age)
+        self.name = name
+        self.gender = gender
+
+    def voice(self):
+        print(f'Привет, я {self.name}')
+
+class Cat(Mammals):
+    def __init__(self, breed):
+        #порода
+        self.breed = breed
+    def voice(self):
+        print('мяу')
+
+class Dog(Mammals):
+    def __init__(self, breed):
+        #порода
+        self.breed = breed
+    def voice(self):
+        print('гав')
+
+Vasya = Human(90, 180, 40, 'Вася', 'male')
+
+print(Vasya.name)
